fd6d6d831dc22a93ffccfb301448fb24c605eb3b
Ted Benson <ted@steamship.com>
2023-05-22 15:21:08 -0400

    Fix

--- src/api.py
+++ src/api.py
@@ -1,73 +1,77 @@
 """Plugin to return Google Image Search images for queries using SerpAPI"""
-from typing import Dict, Type
+from typing import Dict, Type, List
 
 from serpapi import GoogleSearch
-
-from steamship import SteamshipError, Tag
+import requests
+from steamship.invocable import Config, InvocableResponse, InvocationContext
+from steamship.data.block import BlockUploadType
+from steamship import Block, File, MimeTypes, SteamshipError, Tag
 from steamship.invocable import Config, InvocableResponse
-from steamship.plugin.inputs.block_and_tag_plugin_input import BlockAndTagPluginInput
-from steamship.plugin.outputs.block_and_tag_plugin_output import BlockAndTagPluginOutput
+from steamship.plugin.inputs.raw_block_and_tag_plugin_input import RawBlockAndTagPluginInput
+from steamship.plugin.outputs.raw_block_and_tag_plugin_output import RawBlockAndTagPluginOutput
 from steamship.plugin.request import PluginRequest
-from steamship.plugin.tagger import Tagger
+from steamship.plugin.generator import Generator
 
 # tag consts
 TAG_KIND = "search-result"
-TAG_NAME = "GoogleImageSearch"
-VALUE_KEY_QUERY = "query"
-VALUE_KEY_ANSWER = "answer"
+TAG_NAME = "google-image-search"
+SEARCH_ENGINE = "google"
+NUM_IMAGES = 1
 
-
-class SerpApiConfig(Config):
+class GoogleImageSearchGeneratorConfig(Config):
     """Configures the Serp API."""
 
-    serpapi_api_key: str
+    serpapi_api_key: str = ""
 
 
-class SerpApiWrapper(Tagger):
+class GoogleImageSearchGenerator(Generator):
     """Tags files based on search results of their content."""
 
-    config: SerpApiConfig
+    config: GoogleImageSearchGeneratorConfig
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         GoogleSearch.SERP_API_KEY = self.config.serpapi_api_key
 
-    def config_cls(self) -> Type[Config]:
-        """Return the Configuration class."""
-        return SerpApiConfig
+    @classmethod
+    def config_cls(cls) -> Type[Config]:
+        """Return configuration template for the generator."""
+        return GoogleImageSearchGeneratorConfig
 
     def run(
-        self, request: PluginRequest[BlockAndTagPluginInput]
-    ) -> InvocableResponse[BlockAndTagPluginOutput]:
+        self, request: PluginRequest[RawBlockAndTagPluginInput]
+    ) -> InvocableResponse[RawBlockAndTagPluginOutput]:
         """For each block in the submitted file, submit a search query and retrieve the answer."""
 
-        file = request.data.file
-        for block in request.data.file.blocks:
-            search = GoogleSearch({"q": block.text})
+        generated_blocks = []
+        for block in request.data.blocks:
+            if not block.text:
+                continue
+
+            params = {
+                "engine": SEARCH_ENGINE,          # search engine. Google, Bing, Yahoo, Naver, Baidu...
+                "q": block.text,                  # search query
+                "tbm": "isch",                    # image results
+                "num": f"{NUM_IMAGES * 3}",       # number of images per page, with a buffer in case of download failure
+                "ijn": 0,                         # page number: 0 -> first page, 1 -> second...
+            }
+
+            search = GoogleSearch(params)
 
             try:
-                answer = self._answer_from_search_results(search.get_dict())
+                for block in self._answer_from_search_results(search.get_dict()):
+                    generated_blocks.append(block)
+                    
             except ValueError as ve:
                 raise SteamshipError(
                     str=f"Error executing search for {block.text}",
                     suggestion="Please try again if you feel this should have succeeded",
                     error=ve,
                 )
-
-            block.tags.append(
-                Tag(
-                    kind=TAG_KIND,
-                    name=TAG_NAME,
-                    value={
-                        VALUE_KEY_QUERY: block.text,
-                        VALUE_KEY_ANSWER: answer,
-                    },
-                )
-            )
-        return InvocableResponse(data=BlockAndTagPluginOutput(file=file))
+        return InvocableResponse(data=RawBlockAndTagPluginOutput(blocks=generated_blocks))
 
     @staticmethod
-    def _answer_from_search_results(search_result: Dict[str, str]) -> str:
+    def _answer_from_search_results(search_result: Dict[str, str], k: int = 1) -> List[Block]:
         # borrows heavily from the implementation in:
         # https://github.com/hwchase17/langchain/blob/master/langchain/serpapi.py
         # which itself was borrowed from:
@@ -75,37 +79,50 @@ class SerpApiWrapper(Tagger):
         if "error" in search_result.keys():
             raise ValueError(f"Got error from SerpAPI: {search_result['error']}")
 
-        if (
-            "answer_box" in search_result.keys()
-            and "answer" in search_result["answer_box"].keys()
-        ):
-            return search_result["answer_box"]["answer"]
+        ret = []
+        already_seen = {}
 
-        if (
-            "answer_box" in search_result.keys()
-            and "snippet" in search_result["answer_box"].keys()
-        ):
-            return search_result["answer_box"]["snippet"]
+        for image in search_result["images_results"]:
+            image_url = image["original"]
+            if image_url not in already_seen:
+                already_seen[image_url] = True
 
-        if (
-            "answer_box" in search_result.keys()
-            and "snippet_highlighted_words" in search_result["answer_box"].keys()
-        ):
-            return search_result["answer_box"]["snippet_highlighted_words"][0]
+                # We can't do the below method, though I'm preserving it, because
+                # the engine doesn't include a User-Agent header, causing Wikipedia (a common google image
+                # host) to reject the request.
+                #  
+                # ret.append(Block(
+                #     url=image_url, 
+                #     mime_type=MimeTypes.PNG, 
+                #     upload_type=BlockUploadType.URL,
+                #     tags=[
+                #         Tag(
+                #             kind=TAG_KIND,
+                #             name=TAG_NAME
+                #         )
+                #     ]
+                # ))
+            
+                headers = {'User-Agent': 'Steamship Browser v1'}
+                response = requests.get(image_url, headers)
+                if response.ok:
+                    ret.append(Block(
+                        upload_bytes=response.content, 
+                        upload_type=BlockUploadType.FILE,
+                        mime_type=MimeTypes.PNG, 
+                        tags=[
+                            Tag(
+                                kind=TAG_KIND,
+                                name=TAG_NAME
+                            )
+                        ]
+                    ))
 
-        if (
-            "sports_results" in search_result.keys()
-            and "game_spotlight" in search_result["sports_results"].keys()
-        ):
-            return search_result["sports_results"]["game_spotlight"]
-
-        if (
-            "knowledge_graph" in search_result.keys()
-            and "description" in search_result["knowledge_graph"].keys()
-        ):
-            return search_result["knowledge_graph"]["description"]
-
-        if "snippet" in search_result["organic_results"][0].keys():
-            return search_result["organic_results"][0]["snippet"]
-
-        return "No good search result found"
+            # Exit if we've gathered the required number of images
+            if len(ret) == k:
+                return ret
+        
+        if len(ret):
+            return ret
+        
+        raise ValueError(f"No image returned from SerpAPI.")
--- test/test_integration.py
+++ test/test_integration.py
@@ -1,16 +1,16 @@
-"""Integration test for serpapi-wrapper plugin."""
+"""Integration test for google-image-search plugin."""
 from steamship import Steamship
 import api
 
-PLUGIN_HANDLE = "serpapi-wrapper"
+PLUGIN_HANDLE = "google-image-search"
 
 
 def test_run():
 
     test_cases = [
-        ("How old was Abraham Lincoln when he died?", "56 years"),
-        ("What year was the moon landing?", "July 16, 1969"),
-        ("Who was the first president of the United States?", "George Washington"),
+        ("Abraham Lincoln"),
+        ("moon landing"),
+        ("the first president of the United States"),
     ]
 
     with Steamship.temporary_workspace() as client:
@@ -27,6 +27,6 @@ def test_run():
             tag = file.blocks[0].tags[0]
             assert tag.kind == api.TAG_KIND
             assert tag.name == api.TAG_NAME
-            assert len(tag.value.items()) == 2
-            assert tag.value.get(api.VALUE_KEY_QUERY, "") == test_case[0]
-            assert tag.value.get(api.VALUE_KEY_ANSWER, "") == test_case[1]
+            
+            data = file.blocks[0].raw()
+            assert data
